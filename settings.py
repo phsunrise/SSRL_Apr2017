c9223b0ab71bb5fb8a7705ea54dee0fe85e034d1
Peihao Sun <PeihaoSun@dhcp-visitor-enr-118-204.slac.stanford.edu>
2017-04-03 15:32:47 -0700

    initialization

---
+++ settings.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a0 = 3.1648
+a0_reciprocal = 2.*np.pi/a0
+
+plot_dir = "/scratch/users/phsun/SSRL_Nov16/plots/"
+imshape = (195, 487)
+centralpix = (126, 250)
+ar = [23, 193, 205, 296] # active region on the detector, due to slit
+
+spreadsheet_filename = "/scratch/users/phsun/SSRL_Nov16/scans.xlsx"
+
+## hkl histogramming information 
+bins = (201, 201, 201)
+hcoords = np.linspace(1.75, 2.25, bins[0])
+kcoords = np.linspace(1.75, 2.25, bins[1])
+lcoords = np.linspace(-0.25, 0.25, bins[2])
+histrange = ((hcoords[0]*1.5-hcoords[1]*0.5, hcoords[-1]*1.5-hcoords[-2]*0.5),
+             (kcoords[0]*1.5-kcoords[1]*0.5, kcoords[-1]*1.5-kcoords[-2]*0.5),
+             (lcoords[0]*1.5-lcoords[1]*0.5, lcoords[-1]*1.5-lcoords[-2]*0.5))
